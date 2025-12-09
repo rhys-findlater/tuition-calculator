756b621ac78169b69c8ba7a904f4a638183d3c94
rhys-findlater <findlaterrr@gmail.com>
2025-12-10 11:14:37 +1300

    Add Flask app factory and basic templates

---
+++ tuition-calculator/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
